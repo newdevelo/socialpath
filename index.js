37eef4ccfdef1a0eb421c384fb39db981f3886bc
Badr Hammada <105392702+newdevelo@users.noreply.github.com>
2023-03-05 19:59:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
+app.get("/",(req,res) => {
+res.setHeader("Access-control-Allow-Credentials","true"};
+});
+
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
